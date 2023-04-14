b7d3e518995e5276a2a44774cd3e83afe4d8db01
Helio Mesquita <helio.mesquitaios@gmail.com>
2023-04-14 20:18:28 -0300

    add method to handle deeplink

--- MarcoPolo/SceneDelegate.swift
+++ MarcoPolo/SceneDelegate.swift
@@ -7,9 +7,128 @@
 
 import UIKit
 
+extension URL {
+  func pages() -> [String] {
+    var urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: false)
+    urlComponents?.queryItems = []
+
+    var urlString = urlComponents?.url?.absoluteString ?? ""
+    urlString.removeAll(where: { $0 == "?" })
+    return urlString.split(separator: "/").dropFirst().map({ String($0) })
+  }
+}
+
+protocol DeeplinkHandlerProtocol {
+  var path: String { get set }
+  func canOpenURL(_ url: URL) -> Bool
+  func openURL(_ url: URL)
+}
+
+extension DeeplinkHandlerProtocol {
+  func canOpenURL(_ url: URL) -> Bool {
+    url.pages() == path.split(separator: "/").map( { String($0) })
+  }
+}
+
+
+protocol DeeplinkCoordinatorProtocol {
+  @discardableResult func handleURL(_ url: URL) -> Bool
+}
+
+final class AccountDeeplinkHandler: DeeplinkHandlerProtocol {
+
+  var path: String = "account"
+
+  private weak var rootViewController: UIViewController?
+  init(rootViewController: UIViewController?) {
+    self.rootViewController = rootViewController
+  }
+
+  func openURL(_ url: URL) {
+    guard canOpenURL(url) else {
+      return
+    }
+
+    // mock the navigation
+    let viewController = UIViewController()
+    viewController.title = "Account"
+    viewController.view.backgroundColor = .yellow
+    rootViewController?.present(viewController, animated: true)
+  }
+}
+
+
+final class VideoDeeplinkHandler: DeeplinkHandlerProtocol {
+
+  var path: String = "videos"
+
+  private weak var rootViewController: UIViewController?
+  init(rootViewController: UIViewController?) {
+    self.rootViewController = rootViewController
+  }
+
+  // MARK: - DeeplinkHandlerProtocol
+  func openURL(_ url: URL) {
+    guard canOpenURL(url) else {
+      return
+    }
+
+    // mock the navigation
+    let viewController = UIViewController()
+    switch url.path {
+    case "/new":
+      viewController.title = "Video Editing"
+      viewController.view.backgroundColor = .orange
+    default:
+      viewController.title = "Video Listing"
+      viewController.view.backgroundColor = .cyan
+    }
+
+    rootViewController?.present(viewController, animated: true)
+  }
+}
+
+final class DeeplinkCoordinator: DeeplinkCoordinatorProtocol {
+
+  let handlers: [DeeplinkHandlerProtocol]
+
+  init(handlers: [DeeplinkHandlerProtocol]) {
+    self.handlers = handlers
+  }
+
+  @discardableResult func handleURL(_ url: URL) -> Bool{
+    guard let handler = handlers.first(where: { $0.canOpenURL(url) }) else {
+      return false
+    }
+
+    handler.openURL(url)
+    return true
+  }
+}
+
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
+
+  lazy var deeplinkCoordinator: DeeplinkCoordinatorProtocol = {
+    return DeeplinkCoordinator(handlers: [
+      AccountDeeplinkHandler(rootViewController: self.rootViewController),
+      VideoDeeplinkHandler(rootViewController: self.rootViewController)
+    ])
+  }()
+
+  var rootViewController: UIViewController? {
+    return window?.rootViewController
+  }
+
+  func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    guard let firstUrl = URLContexts.first?.url else {
+      return
+    }
+
+    deeplinkCoordinator.handleURL(firstUrl)
+  }
 
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -17,34 +136,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
     guard let _ = (scene as? UIWindowScene) else { return }
-  }
-
-  func sceneDidDisconnect(_ scene: UIScene) {
-    // Called as the scene is being released by the system.
-    // This occurs shortly after the scene enters the background, or when its session is discarded.
-    // Release any resources associated with this scene that can be re-created the next time the scene connects.
-    // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-  }
-
-  func sceneDidBecomeActive(_ scene: UIScene) {
-    // Called when the scene has moved from an inactive state to an active state.
-    // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-  }
-
-  func sceneWillResignActive(_ scene: UIScene) {
-    // Called when the scene will move from an active state to an inactive state.
-    // This may occur due to temporary interruptions (ex. an incoming phone call).
-  }
-
-  func sceneWillEnterForeground(_ scene: UIScene) {
-    // Called as the scene transitions from the background to the foreground.
-    // Use this method to undo the changes made on entering the background.
-  }
-
-  func sceneDidEnterBackground(_ scene: UIScene) {
-    // Called as the scene transitions from the foreground to the background.
-    // Use this method to save data, release shared resources, and store enough scene-specific state information
-    // to restore the scene back to its current state.
   }
 
 
